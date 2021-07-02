61000d4ca9eb0e2ecec1d1142cbe329d0ea684c3
Mitul Mistry <MitulMistryDev@gmail.com>
2021-07-02 11:47:09 -0500

    Fix secret key name in settings

--- journaltool/settings.py
+++ journaltool/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'django-insecure-qoy-q%0!4%@ith32c8((0$yjuajs&jy4b@w*#6k7din1dz($lt'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-qoy-q%0!4%@ith32c8((0$yjuajs&jy4b@w*#6k7din1dz($lt')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-qoy-q%0!4%@ith32c8((0$yjuajs&jy4b@w*#6k7din1dz($lt')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
