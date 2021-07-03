1028c692e3dfdf4cbf604cbbcab9f215dcfd15ae
Mitul Mistry <MitulMistryDev@gmail.com>
2021-07-02 19:09:56 -0500

    Add initial routes and stub in view functions

--- journal/urls.py
+++ journal/urls.py
@@ -3,5 +3,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('register', views.register, name='register'),
+    path('delete', views.delete, name='delete'),
+    path('entries/new', views.new_entry, name='new_entry'),
+    path('entries/<int:id>', views.entry, name='entry'),
+    path('entries/<int:id>/edit', views.edit_entry, name='edit_entry'),
+    path('entries/<int:id>/delete', views.delete_entry, name='delete_entry'),
+    path('distortions', views.distortions, name='distortions'),
+    path('activities/new', views.new_activity, name='new_activity'),
+    path('activities/<int:id>/edit', views.edit_activity, name='edit_activity'),
+    path('activities/<int:id>/delete', views.delete_activity, name='delete_activity'),
+    path('users/<int:id>', views.user_profile, name='user_profile')
 ]
--- journal/views.py
+++ journal/views.py
@@ -4,3 +4,55 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Journal Tool index page.")
+
+
+def login(request):
+    pass
+
+
+def logout(request):
+    pass
+
+
+def register(request):
+    pass
+
+
+def delete(request):
+    pass
+
+
+def new_entry(request):
+    pass
+
+
+def entry(request, id):
+    pass
+
+
+def edit_entry(request, id):
+    pass
+
+
+def delete_entry(request, id):
+    pass
+
+
+def distortions(request):
+    pass
+
+
+def new_activity(request):
+    pass
+
+
+def edit_activity(request, id):
+    pass
+
+
+def delete_activity(request, id):
+    pass
+
+
+def user_profile(request, id):
+    pass
