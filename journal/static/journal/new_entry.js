500086d5e91513e6f5af6b3c41c678cb68a78e5b
Mitul Mistry <MitulMistryDev@gmail.com>
2021-07-04 23:52:01 -0500

    Implement JavaScript functions to set current date and time in new entry form

---
+++ journal/static/journal/new_entry.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    set_current_datetime();
+})
+
+function set_current_datetime() {
+    const input_date = document.querySelector('#input-date');
+    const input_time = document.querySelector('#input-time');    
+    const date = new Date();
+
+    const date_string = `${date.getFullYear()}-${format_zero(date.getMonth() + 1)}-${format_zero(date.getDate())}`;
+
+    input_date.value = date_string;
+    input_date.setAttribute('max', date_string);
+    input_time.value = `${format_zero(date.getHours())}:${format_zero(date.getMinutes())}`;
+}
+
+// Format number to ensure leading zero (1 becomes 01)
+function format_zero(val) {
+    return `${val < 10 ? 0: ''}${val}`;
+}
