099a01f551ceec3178fddccfe892c6af5cdd0a43
Mitul Mistry <MitulMistryDev@gmail.com>
2021-07-05 23:34:01 -0500

    Register models with Django Admin

--- journal/admin.py
+++ journal/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Entry, Activity, Distortion
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Entry)
+admin.site.register(Activity)
+admin.site.register(Distortion)
